331ada6329b4ac1fea0a41fe85fe87d149f8d023
eslamicoo912 <eslamicoo3@gmail.com>
2022-09-21 11:18:00 +0200

    add the routes to the app.js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import "./App.css";
 import Products from "./components/Products/Products.js";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
+import Services from "./components/Services/Services";
+import Team from "./components/Team/Team";
+
 function App() {
   return (
-    <>
-      <Products />
-    </>
+    <div className="app">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/team" element={<Team />} />
+          <Route path="/products" element={<Products />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
