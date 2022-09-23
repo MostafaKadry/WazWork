e83cacb839a10aaea67099533c41f8ba6e85d756
eslamicoo912 <eslamicoo3@gmail.com>
2022-09-23 15:49:51 +0200

    create two sections in the home component

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Team from "./components/Team/Team";
 
 function App() {
   return (
-    <div>
+    <div className="app">
       <Router basename={process.env.PUBLIC_URL}>
         <Navbar />
         <Routes>
